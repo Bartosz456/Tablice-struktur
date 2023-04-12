df4516654d360a885c1140d292626692028e0d09
Bartosz456 <bartoszsz14@gmail.com>
2023-04-12 19:05:08 +0200

    tablice-struktur

---
+++ tablice-struktur.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	struct uzytkownicy {
+		string name;
+		string lname;
+		int id;
+		string kod;
+	};
+
+	uzytkownicy tab[2];
+
+	for (int i = 0; i < 2; i++)
+	{
+		cout << "Podaj imie: ";
+		cin >> tab[i].name;
+		cout << "Podaj nazwisko: ";
+		cin >> tab[i].lname;
+		cout << "Podaj id: ";
+		cin >> tab[i].id;
+		cout << "Podaj kod: ";
+		cin >> tab[i].kod;
+	}
+
+	for (int i = 0; i < 2; i++)
+	{
+		cout << tab[i].name << endl;
+		cout << tab[i].lname << endl;
+		cout << tab[i].id << endl;
+		cout << tab[i].kod << endl;
+	}
+}
